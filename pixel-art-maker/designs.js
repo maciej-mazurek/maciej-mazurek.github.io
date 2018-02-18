1d6c56a084151c183c734a8c08014ca385acd748
Maciej Mazurek <maciej.mazurek@hotmail.com>
2018-02-18 18:22:45 +0100

    function makeGrid

--- pixel-art-maker/designs.js
+++ pixel-art-maker/designs.js
@@ -1,10 +1,29 @@
 // Select color input
+var color = document.getElementById("colorPicker");
 // Select size input
+var size = document.getElementById('sizePicker');
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
+function makeGrid(x, y, n=10000, nsig=2, margin=1.05, cell.size) {
+    var table = document.getElementById("pixel_canvas");
+    var grid = '';
 // Your code goes here!
+    // row loop
+    for (var i = 0; i < height; i++){
+        grid += '<tr class="row">';
+     //cell loop
+        for (var j = 0; j < width; j++){
+            grid += '<td class="cell"></td>';
+        }
+        grid += '</tr>';
+    }
+    // add grid into table
+    table.innerHTML = grid;
+}
+
+    
+    
+    
 
 }
